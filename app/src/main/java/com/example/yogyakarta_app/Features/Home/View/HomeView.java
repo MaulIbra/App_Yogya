109180c02cfabe2500d432243f0ff6016d179d38
maulana <maulibra212@gmail.com>
2020-01-19 21:26:56 +0700

    add home fragment for main menu app

---
+++ app/src/main/java/com/example/yogyakarta_app/Features/Home/View/HomeView.java
@@ -0,0 +1,27 @@
+package com.example.yogyakarta_app.Features.Home.View;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yogyakarta_app.R;
+
+public class HomeView extends Fragment {
+
+
+    public HomeView() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.home_view, container, false);
+    }
+
+}
